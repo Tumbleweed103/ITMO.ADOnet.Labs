49cea8441a1e110a0dd0ee678346416c3f25f5bb
Tumbleweed103 <antatgm@gmail.com>
2021-02-08 23:56:57 +0300

    Add project files.

---
+++ ITMO.ADOnet.Labs/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace DBConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MenuStrip = new System.Windows.Forms.MenuStrip();
+            this.ActionsMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ConnectMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.DisconnectMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AsyncConnectMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MenuStrip
+            // 
+            this.MenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ActionsMenuItem});
+            this.MenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MenuStrip.Name = "MenuStrip";
+            this.MenuStrip.Size = new System.Drawing.Size(302, 28);
+            this.MenuStrip.TabIndex = 0;
+            this.MenuStrip.Text = "Работа с базой данных";
+            // 
+            // ActionsMenuItem
+            // 
+            this.ActionsMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ConnectMenuItem,
+            this.DisconnectMenuItem,
+            this.AsyncConnectMenuItem});
+            this.ActionsMenuItem.Name = "ActionsMenuItem";
+            this.ActionsMenuItem.Size = new System.Drawing.Size(105, 24);
+            this.ActionsMenuItem.Text = "Работа с БД";
+            // 
+            // ConnectMenuItem
+            // 
+            this.ConnectMenuItem.Name = "ConnectMenuItem";
+            this.ConnectMenuItem.Size = new System.Drawing.Size(320, 26);
+            this.ConnectMenuItem.Text = "Подключиться к БД";
+            this.ConnectMenuItem.Click += new System.EventHandler(this.ConnectMenuItem_Click);
+            // 
+            // DisconnectMenuItem
+            // 
+            this.DisconnectMenuItem.Name = "DisconnectMenuItem";
+            this.DisconnectMenuItem.Size = new System.Drawing.Size(320, 26);
+            this.DisconnectMenuItem.Text = "Отключиться от БД";
+            this.DisconnectMenuItem.Click += new System.EventHandler(this.DisconnectMenuItem_Click);
+            // 
+            // AsyncConnectMenuItem
+            // 
+            this.AsyncConnectMenuItem.Name = "AsyncConnectMenuItem";
+            this.AsyncConnectMenuItem.Size = new System.Drawing.Size(320, 26);
+            this.AsyncConnectMenuItem.Text = "Асинхронное подключение к БД";
+            this.AsyncConnectMenuItem.Click += new System.EventHandler(this.AsyncConnectMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 218);
+            this.Controls.Add(this.MenuStrip);
+            this.MainMenuStrip = this.MenuStrip;
+            this.Name = "Form1";
+            this.Text = "Northwind DB";
+            this.MenuStrip.ResumeLayout(false);
+            this.MenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem ActionsMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ConnectMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem DisconnectMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AsyncConnectMenuItem;
+    }
+}
+
---
+++ ITMO.ADOnet.Labs/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DBConnection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connection = new SqlConnection();
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog = Northwind; Integrated Security = True";
+
+        private void ConnectMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.ConnectionString = connectionString;
+                    connection.Open();
+                    MessageBox.Show("Соединение с БД " + connection.Database + " выполнено успешно"
+                        + "\nСервер: " + connection.DataSource);
+                }
+                else
+                {
+                    MessageBox.Show("Соединение с БД уже установлено");
+                }
+            }
+            catch (Exception Xcp)
+            {
+                MessageBox.Show(Xcp.Message, "Unexpected exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void DisconnectMenuItem_Click(object sender, EventArgs e)
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+                MessageBox.Show("Соединение с БД закрыто");
+            }
+            else
+            {
+                MessageBox.Show("Соединение с БД уже закрыто");
+            }
+        }
+
+        private async void AsyncConnectMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.ConnectionString = connectionString;
+                    await connection.OpenAsync();
+                    MessageBox.Show("Соединение с БД " + connection.Database + " выполнено успешно"
+                        + "\nСервер: " + connection.DataSource);
+                }
+                else
+                {
+                    MessageBox.Show("Соединение с БД уже установлено");
+                }
+            }
+            catch (Exception Xcp)
+            {
+                MessageBox.Show(Xcp.Message, "Unexpected exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
