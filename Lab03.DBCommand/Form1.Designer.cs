c014addbb1c25c8855981e7e1c24ec0adc843e5f
Tumbleweed103 <antatgm@gmail.com>
2021-02-16 21:03:22 +0300

    -

--- Lab03.DBCommand/Form1.Designer.cs
+++ Lab03.DBCommand/Form1.Designer.cs
@@ -29,13 +29,76 @@ namespace DBCommand
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sqlConnection = new System.Data.SqlClient.SqlConnection();
+            this.sqlCommand = new System.Data.SqlClient.SqlCommand();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ResultsTextBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sqlConnection
+            // 
+            this.sqlConnection.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=" +
+    "True";
+            this.sqlConnection.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // sqlCommand
+            // 
+            this.sqlCommand.CommandText = "SELECT CustomerID, CompanyName\r\nFROM     Customers";
+            this.sqlCommand.Connection = this.sqlConnection;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(53, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Запрос данных";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ResultsTextBox
+            // 
+            this.ResultsTextBox.Location = new System.Drawing.Point(12, 12);
+            this.ResultsTextBox.Multiline = true;
+            this.ResultsTextBox.Name = "ResultsTextBox";
+            this.ResultsTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.ResultsTextBox.Size = new System.Drawing.Size(419, 251);
+            this.ResultsTextBox.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(221, 278);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Пакетный запрос";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(455, 322);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ResultsTextBox);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "DBCommand";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Data.SqlClient.SqlConnection sqlConnection;
+        private System.Data.SqlClient.SqlCommand sqlCommand;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox ResultsTextBox;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Lab03.DBCommand/Form1.cs
+++ Lab03.DBCommand/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace DBCommand
 {
@@ -15,6 +16,65 @@ namespace DBCommand
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder results = new StringBuilder();
+            using (sqlConnection)
+            {
+                try
+                {
+                    sqlConnection.Open();
+                    SqlDataReader reader = sqlCommand.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            results.Append(reader[i].ToString() + "\t");
+                        }
+                        results.Append(Environment.NewLine);
+                    }
+                    ResultsTextBox.Text = results.ToString();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder results = new StringBuilder();
+            using (sqlConnection)
+            {
+                sqlCommand.CommandText = "SELECT CustomerID, CompanyName FROM Customers; " + "SELECT ProductName, UnitPrice, QuantityPerUnit FROM Products";
+                try
+                {
+                    sqlConnection.Open();
+                    SqlDataReader reader = sqlCommand.ExecuteReader();
+                    bool moreResults = false;
+                    do
+                    {
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                results.Append(reader[i].ToString() + "\t");
+                            }
+                            results.Append(Environment.NewLine);
+                        }
+                        moreResults = reader.NextResult();
+                    }
+                    while (moreResults);
+                    ResultsTextBox.Text = results.ToString();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
