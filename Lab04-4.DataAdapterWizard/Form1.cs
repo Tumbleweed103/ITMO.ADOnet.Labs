cb78b85596ccfc3dcabc13d227a07e8adf9fd2ac
Tumbleweed103 <antatgm@gmail.com>
2021-02-18 13:34:58 +0300

    -

--- Lab04-4.DataAdapterWizard/Form1.cs
+++ Lab04-4.DataAdapterWizard/Form1.cs
@@ -16,5 +16,52 @@ namespace Lab04_4.DataAdapterWizard
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = northwindDataSet1.Customers;
+            sqlDataAdapter1.Fill(northwindDataSet1.Customers);
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            sqlDataAdapter1.Update(northwindDataSet1);
+        }
+
+        private void sqlDataAdapter1_RowUpdating(object sender, System.Data.SqlClient.SqlRowUpdatingEventArgs e)
+        {
+            NorthwindDataSet.CustomersRow CustRow = (NorthwindDataSet.CustomersRow)e.Row;
+            DialogResult response = MessageBox.Show("Continue updating " + 
+                CustRow.CustomerID.ToString() + "?", "Continue?", MessageBoxButtons.YesNo);
+
+            if (response == DialogResult.No)
+            {
+                e.Status = UpdateStatus.SkipCurrentRow;
+            }
+        }
+
+        private void sqlDataAdapter1_RowUpdated(object sender, System.Data.SqlClient.SqlRowUpdatedEventArgs e)
+        {
+            NorthwindDataSet.CustomersRow CustRow = (NorthwindDataSet.CustomersRow)e.Row;
+            MessageBox.Show(CustRow.CustomerID.ToString() + " has been updated");
+
+            northwindDataSet1.Customers.Clear();
+            sqlDataAdapter1.Fill(northwindDataSet1.Customers);
+        }
+
+        private void sqlDataAdapter1_FillError(object sender, FillErrorEventArgs e)
+        {
+            DialogResult response = MessageBox.Show("The following error occured when filling the dataset: " +
+                e.Errors.Message.ToString() + " Continue attempting to fill?", "FillError Encountered", MessageBoxButtons.YesNo);
+
+            if (response == DialogResult.Yes)
+            {
+                e.Continue = true;
+            }
+            else
+            {
+                e.Continue = false;
+            }
+        }
     }
 }
