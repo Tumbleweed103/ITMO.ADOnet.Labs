a233164f69f8f643b566b0c3dca84295d76caf82
Tumbleweed103 <antatgm@gmail.com>
2021-02-18 11:24:12 +0300

    -

--- Lab04-2CreatingDataTable/Form1.Designer.cs
+++ Lab04-2CreatingDataTable/Form1.Designer.cs
@@ -29,21 +29,78 @@ namespace Lab04_2CreatingDataTable
         /// </summary>
         private void InitializeComponent()
         {
+            this.fillChildTableButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.fillParentTableButton = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // fillChildTableButton
+            // 
+            this.fillChildTableButton.Location = new System.Drawing.Point(12, 12);
+            this.fillChildTableButton.Name = "fillChildTableButton";
+            this.fillChildTableButton.Size = new System.Drawing.Size(554, 32);
+            this.fillChildTableButton.TabIndex = 0;
+            this.fillChildTableButton.Text = "Заполнить дочернюю таблицу";
+            this.fillChildTableButton.UseVisualStyleBackColor = true;
+            this.fillChildTableButton.Click += new System.EventHandler(this.fillChildTableButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 51);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(554, 299);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // fillParentTableButton
+            // 
+            this.fillParentTableButton.Location = new System.Drawing.Point(573, 12);
+            this.fillParentTableButton.Name = "fillParentTableButton";
+            this.fillParentTableButton.Size = new System.Drawing.Size(564, 32);
+            this.fillParentTableButton.TabIndex = 2;
+            this.fillParentTableButton.Text = "Заполнить основную таблицу";
+            this.fillParentTableButton.UseVisualStyleBackColor = true;
+            this.fillParentTableButton.Click += new System.EventHandler(this.fillParentTableButton_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(573, 51);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(564, 299);
+            this.dataGridView2.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1149, 362);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.fillParentTableButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.fillChildTableButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
             this.Text = "CreatingDataTable";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button fillChildTableButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button fillParentTableButton;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
 
--- Lab04-2CreatingDataTable/Form1.cs
+++ Lab04-2CreatingDataTable/Form1.cs
@@ -15,6 +15,158 @@ namespace Lab04_2CreatingDataTable
         public Form1()
         {
             InitializeComponent();
+            this.Shown += new EventHandler(Form1_Shown);
+        }
+
+        private DataSet dataSet;
+        private BindingSource bindingSource1;
+        private BindingSource bindingSource2;
+
+        private void MakeParentTable()
+        {
+            DataTable table = new DataTable("ParentTable");
+            DataColumn column;
+            DataRow row;
+
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.Int32");
+            column.ColumnName = "Id";
+            column.ReadOnly = true;
+            column.Unique = true;
+
+            table.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "ParentItem";
+            column.AutoIncrement = false;
+            column.Caption = "ParentItem";
+            column.ReadOnly = false;
+            column.Unique = false;
+
+            table.Columns.Add(column);
+
+            table.Columns.Add("Total", typeof(Double));
+            table.Columns.Add("SalesTax", typeof(Double), "Total * 0.13");
+
+            DataColumn[] PrimaryKeyColumns = new DataColumn[1];
+            PrimaryKeyColumns[0] = table.Columns["Id"];
+            table.PrimaryKey = PrimaryKeyColumns;
+
+            dataSet = new DataSet();
+            dataSet.Tables.Add(table);
+
+            for (int i = 0; i <= 2; i++)
+            {
+                row = table.NewRow();
+                row["Id"] = i;
+                row["ParentItem"] = "ParentItem " + i;
+                row["Total"] = 2 * i + 0.5;
+                table.Rows.Add(row);
+            }
+        }
+
+        private void MakeChildTable()
+        {
+            DataTable table = new DataTable("ChildTable");
+            DataColumn column;
+            DataRow row;
+
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.Int32");
+            column.ColumnName = "ChildId";
+            column.AutoIncrement = true;
+            column.Caption = "Id";
+            column.ReadOnly = true;
+            column.Unique = true;
+
+            table.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "ChildItem";
+            column.AutoIncrement = false;
+            column.Caption = "ChildItem";
+            column.ReadOnly = false;
+            column.Unique = false;
+            table.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.Int32");
+            column.ColumnName = "ParentId";
+            column.AutoIncrement = false;
+            column.Caption = "ParentId";
+            column.ReadOnly = false;
+            column.Unique = false;
+            table.Columns.Add(column);
+
+            dataSet.Tables.Add(table);
+
+            for (int i = 0; i <= 4; i++)
+            {
+                row = table.NewRow();
+                row["ChildId"] = i;
+                row["ChildItem"] = "Item " + i;
+                row["ParentId"] = 0;
+                table.Rows.Add(row);
+            }
+
+            for (int i = 0; i <= 4; i++)
+            {
+                row = table.NewRow();
+                row["ChildId"] = i + 5;
+                row["ChildItem"] = "Item " + i;
+                row["ParentId"] = 1;
+                table.Rows.Add(row);
+            }
+
+            for (int i = 0; i <= 4; i++)
+            {
+                row = table.NewRow();
+                row["ChildId"] = i + 10;
+                row["ChildItem"] = "Item " + i;
+                row["ParentId"] = 2;
+                table.Rows.Add(row);
+            }
+        }
+        private void MakeDataRelation()
+        {
+            DataColumn parentColumn = dataSet.Tables["ParentTable"].Columns["Id"];
+            DataColumn childColumn = dataSet.Tables["ChildTable"].Columns["ParentId"];
+
+            DataRelation relation = new DataRelation("parent2Child", parentColumn, childColumn);
+            dataSet.Tables["ChildTable"].ParentRelations.Add(relation);
+        }
+
+        private void BindToDataGrid()
+        {
+            bindingSource1 = new BindingSource();
+            bindingSource2 = new BindingSource();
+            bindingSource1.DataSource = dataSet.Tables["ChildTable"];
+            bindingSource2.DataSource = dataSet.Tables["ParentTable"];
+        }
+
+        private void fillChildTableButton_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = bindingSource1;
+        }
+
+        private void fillParentTableButton_Click(object sender, EventArgs e)
+        {
+            dataGridView2.DataSource = bindingSource2;
+        }
+
+        private void MakeDataTables()
+        {
+            MakeParentTable();
+            MakeChildTable();
+            MakeDataRelation();
+            BindToDataGrid();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            MakeDataTables();
         }
     }
 }
