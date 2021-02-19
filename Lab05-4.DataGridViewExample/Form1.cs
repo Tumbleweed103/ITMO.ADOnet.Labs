d42ea900a8007051626d3a80648bfe711e3780c0
Tumbleweed103 <antatgm@gmail.com>
2021-02-19 13:33:41 +0300

    -

--- Lab05-4.DataGridViewExample/Form1.cs
+++ Lab05-4.DataGridViewExample/Form1.cs
@@ -16,5 +16,42 @@ namespace Lab05_4.DataGridViewExample
         {
             InitializeComponent();
         }
+
+        private void customersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.customersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.northwindDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.customersTableAdapter.Fill(this.northwindDataSet.Customers);
+            DataColumn Location = new DataColumn("Location");
+            Location.Expression = "City + ',' + Country";
+            northwindDataSet.Customers.Columns.Add(Location);
+        }
+
+        private void AddColumnButton_Click(object sender, EventArgs e)
+        {
+            DataGridViewColumn LocationColumn = new DataGridViewColumn();
+            LocationColumn.Name = "LocationColumn";
+            LocationColumn.HeaderText = "Location";
+            LocationColumn.DataPropertyName = "Location";
+            customersDataGridView.Columns.Add(LocationColumn);
+        }
+
+        private void DeleteColumnButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                customersDataGridView.Columns.Remove("LocationColumn");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
