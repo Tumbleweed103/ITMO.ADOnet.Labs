9dd33aae01429e0825097c8949979db3f57f16ee
Tumbleweed103 <antatgm@gmail.com>
2021-02-19 12:19:13 +0300

    -

--- Lab05-1.DataSourcesWizard/Form1.cs
+++ Lab05-1.DataSourcesWizard/Form1.cs
@@ -16,5 +16,22 @@ namespace Lab05_1.DataSourcesWizard
         {
             InitializeComponent();
         }
+
+        private void customersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.customersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.northwindDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'northwindDataSet.Orders' table. You can move, or remove it, as needed.
+            this.ordersTableAdapter.Fill(this.northwindDataSet.Orders);
+            // TODO: This line of code loads data into the 'northwindDataSet.Customers' table. You can move, or remove it, as needed.
+            this.customersTableAdapter.Fill(this.northwindDataSet.Customers);
+
+        }
     }
 }
