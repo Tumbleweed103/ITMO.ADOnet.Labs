0fd9912e16c30f801cb85313f9b989c2c90777fd
Tumbleweed103 <antatgm@gmail.com>
2021-02-24 16:50:53 +0300

    -

--- Lab08-3_LINQsqlSproc/Form1.cs
+++ Lab08-3_LINQsqlSproc/Form1.cs
@@ -16,5 +16,41 @@ namespace Lab08_3_LINQsqlSproc
         {
             InitializeComponent();
         }
+
+        Northwind db = new Northwind(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog = Northwind; Integrated Security = True");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string param = textBox1.Text;
+            var custQuery = db.CustOrdersDetail(Convert.ToInt32(param));
+            string msg = "";
+
+            foreach (CustOrdersDetailResult custOrdersDetail in custQuery)
+            {
+                msg = msg + custOrdersDetail.ProductName + "\n";
+            }
+            if (msg == "")
+            {
+                msg = "No results.";
+            }
+            MessageBox.Show(msg);
+            param = "";
+            textBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string param = textBox2.Text;
+            var custQuery = db.CustOrderHist(param);
+            string msg = "";
+
+            foreach (CustOrderHistResult custOrdHist in custQuery)
+            {
+                msg = msg + custOrdHist.ProductName + "\n";
+            }
+            MessageBox.Show(msg);
+            param = "";
+            textBox2.Text = "";
+        }
     }
 }
